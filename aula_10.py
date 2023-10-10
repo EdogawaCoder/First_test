762f4c38c2f370d70d2c5dcf6197ac54cbec201b
Edogawa San <142222748+EdogawaCoder@users.noreply.github.com>
2023-10-09 21:13:11 -0300

    Add files via upload

---
+++ aula_10.py
@@ -0,0 +1,30 @@
+matriz_1=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+matriz_2=[
+    [10,11,12],
+    [13,14,15],
+    [16,17,18]
+]
+# elemento=matriz[2][1]
+# print(elemento)
+# num_linhas=len(matriz)
+# num_colunas=len(matriz[0])
+# print(num_colunas, num_linhas)
+
+# matriz_3=[[0,0,0],[0,0,0],[0,0,0]]
+
+# for i in range(len(matriz_1)):
+#     for j in range(len(matriz_1[0])):
+#         matriz_3[i][j]=matriz_1[i][j]*matriz_2[i][j]
+
+# for linha in matriz_3:
+#     print(linha)
+
+contador=5
+
+while contador>0:
+    print(contador)
+    contador-=1
